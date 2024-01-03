5d5335be086d8a7ad03f9d5d6ad827e4055e2b5b
cch137 <96408836+cch137@users.noreply.github.com>
2024-01-03 17:08:59 +0800

    v0.0.10

--- src/server/get-ip.ts
+++ src/server/get-ip.ts
@@ -1,4 +1,20 @@
-export default (req: Request): string => {
+import type { IncomingMessage } from "http"
+
+const singleString = (data: string | string[] | undefined): string => data === undefined ? '' : Array.isArray(data) ? data[0] : data;
+const isRequest = (req: Request | IncomingMessage): req is Request => req instanceof Request;
+const isNotEmptyString = (data: any): data is string => typeof data === 'string' ? /^\s*$/.test(data) : false;
+const trimIps = (ips: string): string => ips.split(',')[0].trim()
+
+export default (req: Request | IncomingMessage): string => {
   // @ts-ignore
-  return (req?.headers['x-forwarded-for'] || req?.headers['x-real-ip'] || req?.ip || '').split(',')[0].trim()
+  const ip = req?.ip;
+  if (isNotEmptyString(ip)) return trimIps(ip);
+
+  if (isRequest(req)) {
+    const headers = req.headers;
+    return trimIps(headers.get('x-forwarded-for') || headers.get('x-real-ip') || '')
+  }
+
+  const headers = req.headers;
+  return trimIps(singleString(headers['x-forwarded-for']) || singleString(headers['x-real-ip']) || '')
 }
