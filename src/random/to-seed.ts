8d478493b849d1515138410597a34581827143b1
cch137 <96408836+cch137@users.noreply.github.com>
2024-01-12 14:36:18 +0800

    v0.0.43

--- src/random/to-seed.ts
+++ src/random/to-seed.ts
@@ -1,21 +1,15 @@
-import sha3 from 'crypto-js/sha3.js'
 import math from '../math'
 import str from '../format/str'
-
-const sha256 = (message: string): string => {
-  return sha3(message, { outputLength: 256 }).toString()
-}
-
-const binaryStrRegex = /0b[0-1]+/i
+import hash from '../format/hash'
 
 export default function toSeed (seed: any): number {
   if (typeof seed === 'number') return Math.round(seed)
-  else if (seed instanceof Object) seed = str(seed)
+  if (seed instanceof Object) seed = str(seed)
   if (typeof seed === 'string') {
-    if (binaryStrRegex.test(seed)) return parseInt(seed.substring(2, seed.length - 1), 2)
+    if (/0b[0-1]+/i.test(seed)) return parseInt(seed.substring(2, seed.length - 1), 2)
     const num = parseInt(seed)
     return Number.isNaN(num)
-      ? math.sum(parseInt(sha256(seed), 16))
+      ? math.sum(parseInt(hash(seed, 256), 16))
       : num
   }
   return Date.now()
