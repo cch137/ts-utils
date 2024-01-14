3b9093162a47b346b13191f0888238f2436e18d1
cch137 <96408836+cch137@users.noreply.github.com>
2024-01-15 02:00:05 +0800

    v0.0.48

--- src/webpage/copy-to-clipboard.ts
+++ src/webpage/copy-to-clipboard.ts
@@ -1,17 +1,12 @@
-export default (text: string) => new Promise((resolve, reject) => {
-  navigator.clipboard.writeText(text)
-    .then(_ => resolve(text))
-    .catch(err => {
-      try {
-        const txArea = document.createElement('textarea')
-        txArea.value = text
-        document.body.appendChild(txArea)
-        txArea.select()
-        document.execCommand('copy')
-        txArea.remove()
-        resolve(text)
-      } catch (er) {
-        reject([err, er])
-      }
-    })
-})
+export default async function copyToClipboard(text: string) {
+  try {
+    await navigator.clipboard.writeText(text)
+  } catch {
+    const txArea = document.createElement('textarea')
+    txArea.value = text
+    document.body.appendChild(txArea)
+    txArea.select()
+    document.execCommand('copy')
+    txArea.remove()
+  }
+}
