88673b2fa7daeaf267a9b3c97ce258fdaefc96fa
cch137 <96408836+cch137@users.noreply.github.com>
2024-01-19 04:48:08 +0800

    v0.0.59

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -1,4 +1,4 @@
-import type { BaseProvider, UniOptions } from "./types";
+import type { BaseProvider, UniOptions, ResponseStream } from "./types";
 
 class SuperProvider<Providers extends Record<string, BaseProvider>> {
   readonly providers: Providers;
@@ -7,8 +7,10 @@ class SuperProvider<Providers extends Record<string, BaseProvider>> {
     this.providers = providers;
   }
 
-  ask(options: UniOptions, model?: keyof Providers) {
-    return this.providers[model || Object.keys(this.providers)[0]].ask(options);
+  ask(options: UniOptions, model?: keyof Providers): ResponseStream
+  ask(question: string, model?: keyof Providers): ResponseStream
+  ask(options: UniOptions | string, model?: keyof Providers) {
+    return this.providers[model || Object.keys(this.providers)[0]].ask(options as UniOptions & string);
   }
 }
 
