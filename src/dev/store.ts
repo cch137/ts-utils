de4bfbe9dd1fbcee4a0f15d1650741fb1c30013b
cch137 <96408836+cch137@users.noreply.github.com>
2024-01-24 03:05:52 +0800

    v0.0.81

--- src/dev/store.ts
+++ src/dev/store.ts
@@ -1,53 +1,37 @@
 const CHANGE = 'change';
 
-type StoreListener<T> = (o: T, k: keyof T, v: any) => any;
+type StoreListener<T> = (o: T, p: StoreType<T>, k: keyof T, v: any) => any;
 
 type StoreType<T> = T & {
   $on: (callback: StoreListener<T>) => () => void;
   $off: (callback: StoreListener<T>) => void;
+  $trigger: () => void;
+  $lock: () => void;
+  $unlock: () => void;
   readonly $object: T;
 }
 
 class StoreChangeEvent<T, K = keyof T, V = any> extends Event {
+  readonly store: StoreType<T>;
   readonly object: T;
-  readonly key: K;
-  readonly value: V;
-  constructor(object: T, key: K, value: V) {
+  readonly key?: K;
+  readonly value?: V;
+  constructor(store: StoreType<T>, key?: K, value?: V) {
     super(CHANGE);
-    this.object = object;
+    this.store = store;
+    this.object = store.$object;
     this.key = key;
     this.value = value;
   }
 }
 
-const checkStoreOriginalObject = (data: object) => {
-  const keys = Object.keys(data);
-  for (const reserved of ['$on', '$off', '$object']) {
-    if (keys.includes('$on')) throw new Error(`Store original object cannot have "${reserved}" key`);
-  }
-}
-
 const store = <T extends object>(data: T): StoreType<T> => {
-  checkStoreOriginalObject(data);
   const et = new EventTarget();
   const listners = new Map<StoreListener<T>, (e: Event) => Promise<void>>();
-  const proxy: StoreType<T> = new Proxy(data, {
-    get(target, key) {
-      if (key === '$on') return $on;
-      if (key === '$off') return $off;
-      if (key === '$object') return {...target};
-      return (target as T)[key as keyof T];
-    },
-    set(target, key, value) {
-      (target as T)[key as keyof T] = value;
-      et.dispatchEvent(new StoreChangeEvent(proxy.$object, key, value));
-      return true;
-    },
-  }) as any;
   const $on = (callback: StoreListener<T>) => {
     const wrappedCallback = async (e: Event | StoreChangeEvent<T>) => {
       if (!(e instanceof StoreChangeEvent)) throw new Error(`Dispatched event is not a instance of ${StoreChangeEvent}`);
-      callback(e.object, e.key, e.value);
+      callback(e.object, e.store, e.key, e.value);
     }
     listners.set(callback, wrappedCallback);
     et.addEventListener(CHANGE, wrappedCallback);
@@ -58,6 +42,28 @@ const store = <T extends object>(data: T): StoreType<T> => {
     listners.delete(callback);
     if (wrappedCallback) et.removeEventListener(CHANGE, wrappedCallback);
   }
+  const $trigger = (key?: keyof T, value?: any) => {
+    et.dispatchEvent(new StoreChangeEvent(proxy, key, value));
+  }
+  let autoTrigger = true;
+  const proxy: StoreType<T> = new Proxy(data, {
+    get(target, key) {
+      switch (key) {
+        case '$on': return $on;
+        case '$off': return $off;
+        case '$trigger': return $trigger;
+        case '$object': return {...target};
+        case '$lock': return () => autoTrigger = false;
+        case '$unlock': return () => autoTrigger = true;
+      }
+      return (target as T)[key as keyof T];
+    },
+    set(target, key, value) {
+      (target as T)[key as keyof T] = value;
+      if (autoTrigger) $trigger(key as keyof T, value);
+      return true;
+    },
+  }) as any;
   return proxy;
 };
 
