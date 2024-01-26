57b465e3d7dfdb4a43ce040a469a5688b27ad496
cch137 <96408836+cch137@users.noreply.github.com>
2024-01-26 16:09:07 +0800

    v0.0.103

--- src/format/version.ts
+++ src/format/version.ts
@@ -1,15 +1,29 @@
 type part = string | number;
 
-export const parse = (s?: string) => new Version(s);
+type VersionLike = string | Version | number[] | string[];
+
+export const parse = (s?: VersionLike) => new Version(s);
 
 export class Version {
+  static parse = parse;
+  static eq = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).eq(parse(rhs));
+  static gte = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).gte(parse(rhs));
+  static gt = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).gt(parse(rhs));
+  static lte = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).lte(parse(rhs));
+  static lt = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).lt(parse(rhs));
+
   readonly length: number;
   major: number;
   minor: number;
   patch: number;
   details: part[];
-  constructor(s: string = '') {
-    const parts = s.split('.');
+
+  constructor(v: VersionLike = '') {
+    const parts = Array.isArray(v)
+      ? v
+      : typeof v === 'string'
+        ? v.split('.')
+        : [v.major, v.minor, v.patch, ...v.details];
     const [major=0, minor=0, patch=0, ...details] = parts;
     this.length = parts.length;
     this.major = Number(major);
@@ -17,11 +31,51 @@ export class Version {
     this.patch = Number(patch);
     this.details = details;
   }
+
   toString() {
     if (this.length === 0) return '';
     return [this.major, this.minor, this.patch, ...this.details].map(s => s ? s.toString(): '').join('.');
   }
-  static parse = parse;
+
+  eq(v: VersionLike) {
+    const { major, minor, patch } = parse(v);
+    if (this.major != major) return false;
+    if (this.minor != minor) return false;
+    if (this.patch != patch) return false;
+    return true;
+  }
+
+  gte(v: VersionLike) {
+    const { major, minor, patch } = parse(v);
+    if (this.major < major) return false;
+    if (this.minor < minor) return false;
+    if (this.patch < patch) return false;
+    return true;
+  }
+
+  gt(v: VersionLike) {
+    const { major, minor, patch } = parse(v);
+    if (this.major <= major) return false;
+    if (this.minor <= minor) return false;
+    if (this.patch <= patch) return false;
+    return true;
+  }
+
+  lte(v: VersionLike) {
+    const { major, minor, patch } = parse(v);
+    if (this.major > major) return false;
+    if (this.minor > minor) return false;
+    if (this.patch > patch) return false;
+    return true;
+  }
+
+  lt(v: VersionLike) {
+    const { major, minor, patch } = parse(v);
+    if (this.major >= major) return false;
+    if (this.minor >= minor) return false;
+    if (this.patch >= patch) return false;
+    return true;
+  }
 }
 
 export default Version;
