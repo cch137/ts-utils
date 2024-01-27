2a63b45fde25b8c7a8c424bf1b3cf3cfbee46098
cch137 <96408836+cch137@users.noreply.github.com>
2024-01-27 21:28:15 +0800

    v0.0.115

--- src/format/version.ts
+++ src/format/version.ts
@@ -1,81 +1,95 @@
 type part = string | number;
 
-type VersionLike = string | Version | part[];
+export type VersionLike = string | VersionBaseObject | VersionObject | part[];
 
-export const parse = (s?: VersionLike) => new Version(s);
-
-export class Version {
-  static parse = parse;
-  static eq = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).eq(parse(rhs));
-  static gte = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).gte(parse(rhs));
-  static gt = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).gt(parse(rhs));
-  static lte = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).lte(parse(rhs));
-  static lt = (lhs: VersionLike, rhs: VersionLike) => parse(lhs).lt(parse(rhs));
-
-  readonly length: number;
+export type VersionBaseObject = {
   major: number;
   minor: number;
   patch: number;
-  details: part[];
-
-  constructor(v: VersionLike = '') {
-    const parts = Array.isArray(v)
-      ? v
-      : typeof v === 'string'
-        ? v ? v.split('.') : []
-        : [v.major, v.minor, v.patch, ...v.details];
-    const [major=0, minor=0, patch=0, ...details] = parts;
-    this.length = Array.isArray(v) ? v.length : parts.length;
-    this.major = Number(major);
-    this.minor = Number(minor);
-    this.patch = Number(patch);
-    this.details = details;
-  }
-
-  toString() {
-    if (this.length === 0) return '';
-    return [this.major, this.minor, this.patch, ...this.details].map(s => s.toString()).join('.');
-  }
-
-  eq(v: VersionLike) {
-    const { major, minor, patch } = parse(v);
-    if (this.major != major) return false;
-    if (this.minor != minor) return false;
-    if (this.patch != patch) return false;
-    return true;
-  }
-
-  gte(v: VersionLike) {
-    const { major, minor, patch } = parse(v);
-    if (this.major < major) return false;
-    if (this.minor < minor) return false;
-    if (this.patch < patch) return false;
-    return true;
-  }
-
-  gt(v: VersionLike) {
-    const { major, minor, patch } = parse(v);
-    if (this.major <= major) return false;
-    if (this.minor <= minor) return false;
-    if (this.patch <= patch) return false;
-    return true;
-  }
-
-  lte(v: VersionLike) {
-    const { major, minor, patch } = parse(v);
-    if (this.major > major) return false;
-    if (this.minor > minor) return false;
-    if (this.patch > patch) return false;
-    return true;
-  }
-
-  lt(v: VersionLike) {
-    const { major, minor, patch } = parse(v);
-    if (this.major >= major) return false;
-    if (this.minor >= minor) return false;
-    if (this.patch >= patch) return false;
-    return true;
-  }
+  details?: part[];
 }
 
-export default Version;
+export type VersionObject = VersionBaseObject & {
+  details: part[];
+  readonly eq: (v: VersionLike) => boolean;
+  readonly gt: (v: VersionLike) => boolean;
+  readonly gte: (v: VersionLike) => boolean;
+  readonly lt: (v: VersionLike) => boolean;
+  readonly lte: (v: VersionLike) => boolean;
+  readonly toString: () => string;
+  readonly parse: (v: VersionLike) => VersionObject;
+}
+
+export const parse = (v?: VersionLike): VersionObject => {
+  const parts = Array.isArray(v)
+    ? v
+    : typeof v === 'string'
+      ? v ? v.split('.') : []
+      : v ? [v.major, v.minor, v.patch, ...(v.details || [])] : [];
+  const [_major=0, _minor=0, _patch=0, ...details] = parts;
+  const length = Array.isArray(v) ? v.length : parts.length;
+  const major = Number(_major);
+  const minor = Number(_minor);
+  const patch = Number(_patch);
+
+  const toString = () => {
+    if (length === 0) return '';
+    return [major, minor, patch, ...details].map(s => s.toString()).join('.');
+  }
+
+  const eq = (v: VersionLike) => {
+    const { major: _1, minor: _2, patch: _3 } = parse(v);
+    if (major != _1) return false;
+    if (minor != _2) return false;
+    if (patch != _3) return false;
+    return true;
+  }
+
+  const gte = (v: VersionLike) => {
+    const { major: _1, minor: _2, patch: _3 } = parse(v);
+    if (major < _1) return false;
+    if (minor < _2) return false;
+    if (patch < _3) return false;
+    return true;
+  }
+
+  const gt = (v: VersionLike) => {
+    const { major: _1, minor: _2, patch: _3 } = parse(v);
+    if (major <= _1) return false;
+    if (minor <= _2) return false;
+    if (patch <= _3) return false;
+    return true;
+  }
+
+  const lte = (v: VersionLike) => {
+    const { major: _1, minor: _2, patch: _3 } = parse(v);
+    if (major > _1) return false;
+    if (minor > _2) return false;
+    if (patch > _3) return false;
+    return true;
+  }
+
+  const lt = (v: VersionLike) => {
+    const { major: _1, minor: _2, patch: _3 } = parse(v);
+    if (major >= _1) return false;
+    if (minor >= _2) return false;
+    if (patch >= _3) return false;
+    return true;
+  }
+
+  return {
+    major,
+    minor,
+    patch,
+    details,
+    eq,
+    gt,
+    gte,
+    lt,
+    lte,
+    toString,
+    parse,
+  }
+};
+
+export const serialize = (v: VersionLike) => parse(v).toString();
