5df6ca19451b3ec195014757386b6e1ad01a006a
cch137 <96408836+cch137@users.noreply.github.com>
2024-05-21 02:25:12 +0800

    feat: collection

---
+++ src/group/collection.ts
@@ -0,0 +1,36 @@
+import Group from ".";
+
+export default class Collection<K = any, V = any> extends Map<K, Group<V>> {
+  group(key: K): Group<V> {
+    if (super.has(key)) return super.get(key)!;
+    const g = new Group<V>();
+    return super.set(key, g), g;
+  }
+
+  once(key: K): Group<V> | undefined {
+    const g = super.get(key);
+    return super.delete(key), g;
+  }
+
+  trim() {
+    [...this].forEach(([k, g]) => {
+      if (g.length === 0) this.delete(k);
+    });
+  }
+}
+
+export class WeakCollection<
+  K extends WeakKey = object,
+  V = any
+> extends WeakMap<K, Group<V>> {
+  group(key: K): Group<V> {
+    if (super.has(key)) return super.get(key)!;
+    const g = new Group<V>();
+    return super.set(key, g), g;
+  }
+
+  once(key: K): Group<V> | undefined {
+    const g = super.get(key);
+    return super.delete(key), g;
+  }
+}
